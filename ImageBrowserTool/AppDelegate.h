3d33fedfc839ba5eccda95547532f4dda7433e9b
inxx <279098907@qq.com>
2016-02-15 10:51:04 +0800

    Initial Commit

---
+++ ImageBrowserTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageBrowserTool
+//
+//  Created by 林祥兴 on 16/2/15.
+//  Copyright © 2016年 pogo.inxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageBrowserTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageBrowserTool
+//
+//  Created by 林祥兴 on 16/2/15.
+//  Copyright © 2016年 pogo.inxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
